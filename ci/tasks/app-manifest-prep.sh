0fceb3e4e19e59a8a4a3764d84e204d04c12f8b7
Awad Awad <email@awadawad.com>
2017-10-13 17:05:29 -0600

    * Added 'deploy-new-version' job to pipeline

---
+++ ci/tasks/app-manifest-prep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#!/bin/bash
+
+set -xe
+
+cat ./current-app-info/current-app.txt
+
+sed "s/APPNAME/$CF_APP_PREFIX-$(cat ./current-app-info/next-app.txt)/" ./attendee-service-source/manifests/manifest.yml > ./app-manifest-output/manifest.yml
+
+cat ./app-manifest-output/manifest.yml
